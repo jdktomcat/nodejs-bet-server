24855274aec96487778d5920a7e234adc2042fd5
andrew.li <andrew.li@tron.network>
2020-01-20 14:21:39 +0800

    update

--- tronbet_event/src/utils/common.js
+++ tronbet_event/src/utils/common.js
@@ -319,8 +319,7 @@ async function randomBMW() {
     // 暂时屏蔽掉
     let luckyNum = await getRandomInt(1, 100000)
     // 概率 100 / 100000  大概是 1/1000
-    // if (luckyNum <= 100){
-    if (luckyNum >= 1000){
+    if (luckyNum <= 100){
         return true
     }
     
