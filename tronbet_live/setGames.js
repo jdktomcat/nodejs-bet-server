6da9f88ac9163480c4f1f4c8a220c7a0157336ca
andrew.li <andrew.li@tron.network>
2020-02-03 17:02:53 +0800

    dice 1.5倍

--- tronbet_live/setGames.js
+++ tronbet_live/setGames.js
@@ -1,16 +1,19 @@
-const redisUtil = require("./src/utils/redisUtil");
-const axios = require("axios");
-const game = require("./src/service/games");
+// const redisUtil = require("./src/utils/redisUtil");
+// const axios = require("axios");
+// const game = require("./src/service/games");
+//
+// async function gameGames() {
+//   let games = await game.parseGames();
+//   await redisUtil.hset("tronlive:gamelist", "games", JSON.stringify(games));
+//   console.log(games);
+// }
+//
+// async function main() {
+//   await gameGames();
+//   process.exit(0);
+// }
+//
+// main();
 
-async function gameGames() {
-  let games = await game.parseGames();
-  await redisUtil.hset("tronlive:gamelist", "games", JSON.stringify(games));
-  console.log(games);
-}
 
-async function main() {
-  await gameGames();
-  process.exit(0);
-}
-
-main();
+require('./setDiceAddition')
