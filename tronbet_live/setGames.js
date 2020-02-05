4dac68d29ac47b1e98b12a6be968f4e403a1fe51
andrew.li <andrew.li@tron.network>
2020-02-05 18:49:49 +0800

    update test

--- tronbet_live/setGames.js
+++ tronbet_live/setGames.js
@@ -16,4 +16,19 @@
 // main();
 
 
-require('./setDiceAddition')
+const db = require("./src/utils/dbUtil");
+async function fixBalance() {
+    let sqlbefore = "select * from live_balance where addr = '0x5b6abfaa7a32f2e5ab35b6918c93f4593a311b1f' and currency = 'ETH'";
+    let rs1 = await db.exec(sqlbefore)
+    console.log("before is --->",rs1)
+    //
+    let sql =
+    "update live_balance set balance = balance + ? where addr = '0x5b6abfaa7a32f2e5ab35b6918c93f4593a311b1f' and currency = 'ETH'";
+    let balance = 0.48 * 1e9
+    let res = await db.exec(sql, [balance]);
+    console.log("update res->",res)
+    //
+    let rs2 = await db.exec(sqlbefore)
+    console.log("after is --->",rs2)
+}
+fixBalance()
