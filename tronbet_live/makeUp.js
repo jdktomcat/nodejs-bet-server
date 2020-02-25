04e7da5fa146ce40d0cd2787b954955d3411aab3
andrew.li <andrew.li@tron.network>
2020-02-26 00:38:42 +0800

    运维已处理，移除调用

--- tronbet_live/makeUp.js
+++ tronbet_live/makeUp.js
@@ -16,9 +16,9 @@ async function addData2AirDrop(sql) {
 async function makeUpLiveToken() {
     // update 20200220
     let users = [
-        {"addr": "TAqqSP1FK9yXJjnD4kMjXxKMN6PAfYktu4", "amount": 137826.491, "adAmount": 368.52 * 1000000},
-        {"addr": "TDadzsjnk6VQeHeKLCanqHPoZ5qdp1kioV", "amount": 743683.34, "adAmount": 1988.45813 * 1000000},
-        {"addr": "TBzw5164Bi16TSrTFvLdxBPk3cJDCMcxEx", "amount": 29920, "adAmount": 80 * 1000000},
+    //     {"addr": "TAqqSP1FK9yXJjnD4kMjXxKMN6PAfYktu4", "amount": 137826.491, "adAmount": 368.52 * 1000000},
+    //     {"addr": "TDadzsjnk6VQeHeKLCanqHPoZ5qdp1kioV", "amount": 743683.34, "adAmount": 1988.45813 * 1000000},
+    //     {"addr": "TBzw5164Bi16TSrTFvLdxBPk3cJDCMcxEx", "amount": 29920, "adAmount": 80 * 1000000},
     ]
     for (let one of users) {
         let startTs = await getMaxEndTs(one.addr);
@@ -36,27 +36,3 @@ async function makeUpLiveToken() {
 // }
 //
 // main();
-
-
-async function alterTable() {
-    const sql = "ALTER TABLE tron_live.live_online_game ADD is_new VARCHAR (1) DEFAULT '0' "
-    let res = await db.exec(sql, []);
-    console.log(sql)
-    console.log("res is ",res)
-    //
-    const sql2 = `CREATE TABLE tron_bet_admin.sum_addr_detail (
-        id int(11) NOT NULL AUTO_INCREMENT,
-        day varchar(20) DEFAULT NULL,
-        addr varchar(255) DEFAULT NULL,
-        ts bigint(20) DEFAULT NULL,
-        PRIMARY KEY (id),
-        KEY sum_dice_data_index1 (day)
-    )`
-    let res2 = await db.exec(sql2, []);
-    console.log(sql2)
-    console.log("res is ",res2)
-}
-
-alterTable().then(e=>{
-    process.exit(0);
-})
