23775e6f2292f2ca97f235e4aeb35296ab44b5db
andrew.li <andrew.li@tron.network>
2020-02-29 19:47:59 +0800

    update

--- tronbet_live/makeUp.js
+++ tronbet_live/makeUp.js
@@ -30,13 +30,13 @@ async function makeUpLiveToken() {
 
 const fixBalance = async function () {
     const array = [
-        {addr: "TPUMuJZkokoXHxfXCjoerXnybBtg9LS8hT", fix: 683 * 1e6},
-        {addr: "TQnckPMjwkP2uYNW26euwxgDhdzTBoWuPt", fix: 3839 * 1e6},
-        {addr: "TN6rWvxATrd1XywjHceDYm2UxS7khegErV", fix: 1300 * 1e6},
-        {addr: "TCVvvBLJLe53F3rz9vmHEvdpMvTCK4TmxS", fix: 170 * 1e6},
-        {addr: "TDUofcLnJoLotdt7JKhjTgN5N4ZRHCD8m1", fix: 17760.03222 * 1e6},
-        {addr: "TAKYEtb9xtxG5czmkvvsmvF8ui1Y4Sn8cD", fix: 14593.34716 * 1e6},
-        {addr: "TUURWq51iQ9Yt5XrGpJgXk8rJUGm8Qn5YE", fix: 20032 * 1e6},
+        // {addr: "TPUMuJZkokoXHxfXCjoerXnybBtg9LS8hT", fix: 683 * 1e6},
+        // {addr: "TQnckPMjwkP2uYNW26euwxgDhdzTBoWuPt", fix: 3839 * 1e6},
+        // {addr: "TN6rWvxATrd1XywjHceDYm2UxS7khegErV", fix: 1300 * 1e6},
+        // {addr: "TCVvvBLJLe53F3rz9vmHEvdpMvTCK4TmxS", fix: 170 * 1e6},
+        // {addr: "TDUofcLnJoLotdt7JKhjTgN5N4ZRHCD8m1", fix: 17760.03222 * 1e6},
+        // {addr: "TAKYEtb9xtxG5czmkvvsmvF8ui1Y4Sn8cD", fix: 14593.34716 * 1e6},
+        // {addr: "TUURWq51iQ9Yt5XrGpJgXk8rJUGm8Qn5YE", fix: 20032 * 1e6},
     ]
     for (let e of array) {
         const updateSql = "update tron_live.live_balance set balance = balance + ? where addr = ? and currency = 'TRX' "
--- tronbet_live/setGames.js
+++ tronbet_live/setGames.js
@@ -15,17 +15,44 @@
 //
 // main();
 
-
+const db = require("./src/utils/dbUtil");
 const removeDirtyGame = async function () {
-    const db = require("./src/utils/dbUtil");
-    const updateSql = "delete from live_online_game where status = '1'"
+    const updateSql = "delete from tron_live.live_online_game where status = '1'"
     console.log("excute sql is: ",updateSql)
     await db.exec(updateSql, [])
+}
+
+// TGT6C12BP36s21n5MTwG9Fcxa597UvKDRs  140
+// TSo2tj2YXfMhLHpb6GxFombCuErtJrJ8nD  127.74
+// TJuJFibqBZGB13ydFCH4AK8zhK18r6dD6k 14050
+const fixBalance = async function () {
+    const array = ['TGT6C12BP36s21n5MTwG9Fcxa597UvKDRs','TSo2tj2YXfMhLHpb6GxFombCuErtJrJ8nD','TJuJFibqBZGB13ydFCH4AK8zhK18r6dD6k']
+    const querySql = "select addr,balance / 1000000 as balance,currency from tron_live.live_balance where addr = ? and currency = 'TRX'"
+    const updateSql = "update tron_live.live_balance set balance = 0 where addr = ? and currency = 'TRX' "
+    for (let e of array) {
+        console.log(querySql,e)
+        const a1 = await db.exec(querySql,[e])
+        if(a1.length > 0){
+            console.log(`${e} data is `,a1.length,a1[0].addr,a1[0].balance,a1[0].currency)
+        }
+        console.log(updateSql,e)
+        await db.exec(updateSql,[e])
+    }
+    console.log("\n\nafter is===>")
+    for (let e of array) {
+        console.log(querySql,e)
+        const a1 = await db.exec(querySql,[e])
+        if(a1.length > 0){
+            console.log(`${e} data is `,a1.length,a1[0].addr,a1[0].balance,a1[0].currency)
+        }
+    }
 }
 
 
 async function main() {
     await removeDirtyGame()
+    console.log("======分割线=====\n\n")
+    await fixBalance()
     console.log("remove gameID Done");
     process.exit(0);
 }
