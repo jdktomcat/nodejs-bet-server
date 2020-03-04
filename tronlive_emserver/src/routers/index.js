dcf246a183d349995f084e48835ae66333aece03
andrew.li <andrew.li@tron.network>
2020-03-04 19:25:05 +0800

    update

--- tronlive_emserver/src/routers/index.js
+++ tronlive_emserver/src/routers/index.js
@@ -1,7 +1,7 @@
 const router = require('koa-router')()
 
 const action = require('./action')
-const platius = require('./platius')
+const platius = require('./action')
 
 router.use('/', action.routes(), action.allowedMethods())
 router.use('/platius', platius.routes(), platius.allowedMethods())
--- tronlive_emserver/src/routers/platius.js
+++ tronlive_emserver/src/routers/platius.js
@@ -3,12 +3,21 @@
  */
 
 const router = require('koa-router')()
-const action = require('./../service/newPlatius')
+// const action = require('./../service/newPlatapi')
 
-module.exports = router.post( '', action.All)
-module.exports = router.post( 'GetAccount', action.GetAccount)
-module.exports = router.post( 'Result', action.Result)
-module.exports = router.post( 'Wager', action.Wager)
-module.exports = router.post( 'GetBalance', action.GetBalance)
-module.exports = router.post( 'Rollback', action.RollBack)
-module.exports = router.post( 'GetTransactionStatus', action.GetTransactionStatus)
+const test = function(ctx){
+    console.log("66666666")
+    ctx.body = 200
+}
+
+
+module.exports = router.post( '/', test)
+
+
+// module.exports = router.post( '', action.All)
+// module.exports = router.post( 'GetAccount', action.GetAccount)
+// module.exports = router.post( 'Result', action.Result)
+// module.exports = router.post( 'Wager', action.Wager)
+// module.exports = router.post( 'GetBalance', action.GetBalance)
+// module.exports = router.post( 'Rollback', action.RollBack)
+// module.exports = router.post( 'GetTransactionStatus', action.GetTransactionStatus)
