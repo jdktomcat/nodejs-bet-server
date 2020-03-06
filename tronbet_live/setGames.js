4e759f10213feaac66fdefcf562a1e085d63ed7b
andrew.li <andrew.li@tron.network>
2020-03-06 15:36:28 +0800

    update

--- tronbet_live/setGames.js
+++ tronbet_live/setGames.js
@@ -1,18 +1,19 @@
 const db = require("./src/utils/dbUtil");
 
 const queryBalance = async function (array) {
-    const sql = `select * from tron_live.live_balance where addr = ? and currency = ?`
+    const sql = `select * from live_balance where uid = ? and currency = ?`
     for (let e of array) {
-        const a = await db.exec(sql, [e.addr, e.currency])
-        console.log(sql, JSON.stringify(e), ' and result is', a)
+        const p = [e.uid,e.currency]
+        const a = await db.exec(sql, p)
+        console.log(sql, p, ' and result is', a)
     }
 }
 
 
 const updateAddBalance = async function (array) {
     for (let e of array) {
-        const updateSql = "update tron_live.live_balance set balance = balance + ? where addr = ? and currency = ? "
-        const params = [e.fix, e.addr, e.fix]
+        const updateSql = "update tron_live.live_balance set balance = balance + ? where uid = ? and currency = ? "
+        const params = [e.fix, e.uid, e.currency]
         console.log(updateSql, params)
         await db.exec(updateSql, params);
     }
@@ -21,9 +22,9 @@ const updateAddBalance = async function (array) {
 
 const fixBalance = async function () {
     const array = [
-        {addr: "LRPTZ7aWqZJpeaTnvcUUQKhnhotfL5wyBN", fix: 1.21 * 1e9, currency: "LTC"},
-        {addr: "bnb136ns6lfw4zs5hg4n85vdthaad7hq5m4gtkgf23", fix: 16.825 * 1e9, currency: "BNB"},
-        {addr: "bnb136ns6lfw4zs5hg4n85vdthaad7hq5m4gtkgf23", fix: 1.377965 * 1e9, currency: "BNB"},
+        {uid:'50957',addr: "LRPTZ7aWqZJpeaTnvcUUQKhnhotfL5wyBN", fix: 1.21 * 1e9, currency: "LTC", email:"cokekatum@gmail.com"},
+        {uid:'42869',addr: "bnb136ns6lfw4zs5hg4n85vdthaad7hq5m4gtkgf23", fix: 16.825 * 1e9, currency: "BNB", email:"gfbrown1411@gmail.com"},
+        {uid:'46585',addr: "bnb136ns6lfw4zs5hg4n85vdthaad7hq5m4gtkgf23", fix: 1.377965 * 1e9, currency: "BNB", email:"duonglonganh@gmail.com"},
     ]
     await queryBalance(array)
     //
