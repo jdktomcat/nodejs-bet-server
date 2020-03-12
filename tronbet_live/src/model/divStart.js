dabd2cb237e6c15c3dfd0ec239edc414c15b6cf8
andrew.li <andrew.li@tron.network>
2020-03-12 23:49:36 +0800

    新增处理

--- tronbet_live/src/model/divStart.js
+++ tronbet_live/src/model/divStart.js
@@ -24,6 +24,13 @@ const raw = async function (sql, params) {
     return data
 }
 
+const queryDetail = async function () {
+    let sql = 'select send_ts from tron_live.live_div_info order by round desc limit 1';
+    const data = await raw(sql, [])
+    const a = data[0].send_ts
+    const ts = a * 1000
+    return ts
+}
 
 const queryDiv = async function (div_state) {
     let sql = 'select send_ts from tron_live.live_div_info order by round desc limit 1';
@@ -36,6 +43,17 @@ const queryDiv = async function (div_state) {
     return ts
 }
 
+const queryDivIsOver = async function () {
+    let sql = 'select div_state from tron_live.live_div_info order by round desc limit 1';
+    const data = await raw(sql, [])
+    const a = data[0].div_state || '1'
+    if(a === '1'){
+        return true
+    }else {
+        return false
+    }
+}
+
 const compareDate = async function (div_state) {
     const date1 = await queryDiv(div_state)
     let start = new Date();
@@ -43,6 +61,15 @@ const compareDate = async function (div_state) {
     start.setUTCSeconds(0)
     start.setUTCMilliseconds(0)
     const now = start.getTime()
+    //
+    if(div_state === 3){
+        //20分钟特殊处理,每过20分钟还是没完成，直接重启
+        const isReStart = queryDivIsOver()
+        console.log("debug20livestart--->",new Date())
+        if(isReStart){
+            await reStartLiveDiv()
+        }
+    }
     if (date1 < now) {
         await reStartLiveDiv()
     } else {
@@ -59,12 +86,20 @@ const divSchedule = function () {
     const a2 = schedule.scheduleJob('*/15 3-4 * * *', async function () {
         await compareDate('2')
     })
+    //
+    const a3 = schedule.scheduleJob('*/20 3-4 * * *', async function () {
+        await compareDate('3')
+    })
     // 15点，即23点profit的时候
     const b1 = schedule.scheduleJob('*/10 15-16 * * *', async function () {
         await compareDate('1')
     })
     const b2 = schedule.scheduleJob('*/15 15-16 * * *', async function () {
         await compareDate('2')
+    })
+    //
+    const b3 = schedule.scheduleJob('*/20 15-16 * * *', async function () {
+        await compareDate('3')
     })
 }
 
