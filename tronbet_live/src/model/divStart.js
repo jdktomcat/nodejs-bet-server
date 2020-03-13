75e87a16577d1f23420388d46a98695605dcc97f
andrew.li <andrew.li@tron.network>
2020-03-13 23:51:40 +0800

    update

--- tronbet_live/src/model/divStart.js
+++ tronbet_live/src/model/divStart.js
@@ -1,6 +1,6 @@
 const {spawn} = require("child_process");
 const reStartLiveDiv = async function () {
-    console.log("---->retsart live_div");
+    console.log("----pm2restart---->",new Date());
     const ls = spawn("pm2", ["restart", "tronlive_dividends"]);
     ls.stdout.on("data", data => {
         console.log(`livetrxRestart输出：${data}`);
@@ -44,6 +44,31 @@ const queryDivIsOver = async function () {
     }
 }
 
+const queryDivDetailNumber = async function () {
+    let sql = 'select round,div_state from tron_live.live_div_info order by round desc limit 1';
+    const data = await raw(sql, [])
+    const a = data[0].div_state || '1'
+    if (a === '1') {
+        const roundTmp = data[0].round
+        const round = Number(roundTmp)
+        if (!isNaN(round)) {
+            let sql = 'select count(1) as count from live_div_detail where round = ?';
+            const countInfo = await raw(sql, [round])
+            const count = countInfo[0].count || 0
+            const min = new Date().getUTCMinutes()
+            console.log("live_div_detail count is: ", count, ', min is', min)
+            if (min > 6 && min <= 12 && count <= 2750) {
+                return true
+            } else if (min > 12 && min < 18 && count <= 5000) {
+                return true
+            } else if (min >= 18 && min < 23 && count <= 9000) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
 const isDivNormal = async function () {
     const date1 = await queryDiv()
     let start = new Date();
@@ -62,10 +87,17 @@ const isDivNormal = async function () {
 const compareDate = async function () {
     const isStart = isDivNormal()
     if (isStart) {
-        //15分钟后判断是否完成，or restart
         const min = new Date().getUTCMinutes()
-        if (min >= 13) {
+        if (min > 6 && min <= 23) {
+            const detailNumerNormal = await queryDivDetailNumber()
+            console.log(min, " min,detailNumerNormal is", detailNumerNormal)
+            if (detailNumerNormal) {
+                await reStartLiveDiv()
+            }
+        } else if (min >= 23 && min <= 59) {
+            //25分钟后判断是否完成，or restart
             const isNotOver = await queryDivIsOver()
+            console.log("div isNotOver is", isNotOver)
             if (isNotOver) {
                 await reStartLiveDiv()
             }
