457573326f1658c09035f0a009d350045eeab4fa
andrew.li <andrew.li@tron.network>
2020-03-21 19:48:24 +0800

    update

--- tronbet_live/src/configs/blacklist.js
+++ tronbet_live/src/configs/blacklist.js
@@ -2027,6 +2027,7 @@ const blacklist = {
   "TMsqFpUHFcDhvJw5GL8yrSapqwz5LJDgx4": true,
   "TNP948Pk3BqqvjAUB5WRFJwtcTntEo1YUR": true,
   "TGrRyySMpAmh4iZ9A9PnfmuqmVDrUREgow": true,
+  "TRjG49QTLNqu9CESrp7f2QBVjDJf4XQpkC": true,
 }
 
 module.exports = blacklist;
