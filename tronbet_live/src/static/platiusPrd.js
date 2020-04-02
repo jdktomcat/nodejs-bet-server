90b67118f418937d0ad90ce40974125854454c49
andrew.li <andrew.li@tron.network>
2020-04-02 14:55:18 +0800

    update

--- tronbet_live/src/static/platiusPrd.js
+++ tronbet_live/src/static/platiusPrd.js
@@ -3,329 +3,329 @@ module.exports  = [
         "launchUrl": "https://wink.empiregamings.com/wink/piratesmap/launch",
         "name": "Pirates Map",
         "gameID": "532",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/piratesmap.jpg",
+        "png": "//www.empiregamings.com/img/games/piratesmap.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/jackpotlab/launch",
         "name": "Jackpot Lab",
         "gameID": "531",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/jackpotlab.jpg",
+        "png": "//www.empiregamings.com/img/games/jackpotlab.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/azteccoins/launch",
         "name": "Aztec Coins",
         "gameID": "529",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/azteccoins.jpg",
+        "png": "//www.empiregamings.com/img/games/azteccoins.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/chinesetigers/launch",
         "name": "Chinese Tigers",
         "gameID": "530",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/chinesetigers.jpg",
+        "png": "//www.empiregamings.com/img/games/chinesetigers.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/rhinomania/launch",
         "name": "Rhino Mania",
         "gameID": "528",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/rhinomania.jpg",
+        "png": "//www.empiregamings.com/img/games/rhinomania.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/webbyheroes/launch",
         "name": "Webby Heroes",
         "gameID": "526",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/webbyheroes.jpg",
+        "png": "//www.empiregamings.com/img/games/webbyheroes.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/luckycat/launch",
         "name": "Lucky Cat",
         "gameID": "527",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/luckycat.jpg",
+        "png": "//www.empiregamings.com/img/games/luckycat.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/pharaohsempire/launch",
         "name": "Pharaoh's Empire",
         "gameID": "492",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/pharaohsempire.jpg",
+        "png": "//www.empiregamings.com/img/games/pharaohsempire.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/bisontrail/launch",
         "name": "Bison Trail",
         "gameID": "491",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/bisontrail.jpg",
+        "png": "//www.empiregamings.com/img/games/bisontrail.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/jadevalley/launch",
         "name": "Jade Valley",
         "gameID": "485",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/jadevalley.jpg",
+        "png": "//www.empiregamings.com/img/games/jadevalley.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/neonclassic/launch",
         "name": "Neon Classic",
         "gameID": "488",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/neonclassic.jpg",
+        "png": "//www.empiregamings.com/img/games/neonclassic.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/powerofposeidon/launch",
         "name": "Power of Poseidon",
         "gameID": "452",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/powerofposeidon.jpg",
+        "png": "//www.empiregamings.com/img/games/powerofposeidon.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/greatocean/launch",
         "name": "Great Ocean",
         "gameID": "486",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/greatocean.jpg",
+        "png": "//www.empiregamings.com/img/games/greatocean.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/megadrago/launch",
         "name": "Mega Drago",
         "gameID": "450",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/megadrago.jpg",
+        "png": "//www.empiregamings.com/img/games/megadrago.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/legendofatlantis/launch",
         "name": "Legend of Atlantis",
         "gameID": "442",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/atlantis.jpg",
+        "png": "//www.empiregamings.com/img/games/atlantis.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/aztectemple/launch",
         "name": "Aztec Temple",
         "gameID": "448",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/aztectemple.jpg",
+        "png": "//www.empiregamings.com/img/games/aztectemple.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/tripledragon/launch",
         "name": "Triple Dragon",
         "gameID": "444",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/tripledragon.jpg",
+        "png": "//www.empiregamings.com/img/games/tripledragon.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/bookofegypt/launch",
         "name": "Book of Egypt",
         "gameID": "465",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/bookofegypt.jpg",
+        "png": "//www.empiregamings.com/img/games/bookofegypt.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/monkeysjourney/launch",
         "name": "Monkeys Journey",
         "gameID": "480",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/monkeysjourney.jpg",
+        "png": "//www.empiregamings.com/img/games/monkeysjourney.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/sakurawind/launch",
         "name": "Sakura Wind",
         "gameID": "469",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/sakurawind.jpg",
+        "png": "//www.empiregamings.com/img/games/sakurawind.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/luckymoney/launch",
         "name": "Lucky Money",
         "gameID": "475",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/luckymoney.jpg",
+        "png": "//www.empiregamings.com/img/games/luckymoney.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/loveis/launch",
         "name": "Love is",
         "gameID": "476",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/loveis.jpg",
+        "png": "//www.empiregamings.com/img/games/loveis.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/cinderella/launch",
         "name": "Cinderella",
         "gameID": "477",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/cinderella.jpg",
+        "png": "//www.empiregamings.com/img/games/cinderella.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/crystalsevens/launch",
         "name": "Crystal Sevens",
         "gameID": "425",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/crystalsevens.jpg",
+        "png": "//www.empiregamings.com/img/games/crystalsevens.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/magicalmirror/launch",
         "name": "Magical Mirror",
         "gameID": "446",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/magicalmirror.jpg",
+        "png": "//www.empiregamings.com/img/games/magicalmirror.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/powerofgods/launch",
         "name": "Power of Gods",
         "gameID": "483",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/powerofgods.jpg",
+        "png": "//www.empiregamings.com/img/games/powerofgods.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/crazyjelly/launch",
         "name": "Crazy Jelly",
         "gameID": "443",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/crazyjelly.jpg",
+        "png": "//www.empiregamings.com/img/games/crazyjelly.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/luckydolphin/launch",
         "name": "Lucky Dolphin",
         "gameID": "427",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/luckydolphin.jpg",
+        "png": "//www.empiregamings.com/img/games/luckydolphin.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/richywitchy/launch",
         "name": "Richy Witchy",
         "gameID": "429",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/richywitchy.jpg",
+        "png": "//www.empiregamings.com/img/games/richywitchy.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/magicalwolf/launch",
         "name": "Magical Wolf",
         "gameID": "394",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/magicalwolf.jpg",
+        "png": "//www.empiregamings.com/img/games/magicalwolf.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/mistressofamazon/launch",
         "name": "Mistress of Amazon",
         "gameID": "393",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/mistressofamazon.jpg",
+        "png": "//www.empiregamings.com/img/games/mistressofamazon.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/arabiantales/launch",
         "name": "Arabian Tales",
         "gameID": "417",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/arabiannights.jpg",
+        "png": "//www.empiregamings.com/img/games/arabiannights.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/junglespin/launch",
         "name": "Jungle Spin",
         "gameID": "423",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/junglespin.jpg",
+        "png": "//www.empiregamings.com/img/games/junglespin.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/fruitysevens/launch",
         "name": "Fruity Sevens",
         "gameID": "424",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/fruitysevens.jpg",
+        "png": "//www.empiregamings.com/img/games/fruitysevens.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/juicyspins/launch",
         "name": "Juicy Spins",
         "gameID": "428",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/juicyspins.jpg",
+        "png": "//www.empiregamings.com/img/games/juicyspins.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/crocoman/launch",
         "name": "Crocoman",
         "gameID": "409",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/crocoman.jpg",
+        "png": "//www.empiregamings.com/img/games/crocoman.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/safariadventures/launch",
         "name": "Safari Adventures",
         "gameID": "426",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/safari.jpg",
+        "png": "//www.empiregamings.com/img/games/safari.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/fairyforest/launch",
         "name": "Fairy Forest",
         "gameID": "400",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/fairyforest.jpg",
+        "png": "//www.empiregamings.com/img/games/fairyforest.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/princessofbirds/launch",
         "name": "Princess of Birds",
         "gameID": "401",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/princessofbirds.jpg",
+        "png": "//www.empiregamings.com/img/games/princessofbirds.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/jewelbang/launch",
         "name": "Jewel Bang",
         "gameID": "395",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/jewelbang.jpg",
+        "png": "//www.empiregamings.com/img/games/jewelbang.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/fieryplanet/launch",
         "name": "Fiery Planet",
         "gameID": "392",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/fieryplanet.jpg",
+        "png": "//www.empiregamings.com/img/games/fieryplanet.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/cleosgold/launch",
         "name": "Cleo's Gold",
         "gameID": "386",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/cleosgold.jpg",
+        "png": "//www.empiregamings.com/img/games/cleosgold.jpg",
         "type": "slots"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/baccarat/launch",
         "name": "Baccarat Pro",
         "gameID": "99",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/baccarat.jpg",
+        "png": "//www.empiregamings.com/img/games/baccarat.jpg",
         "type": "table"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/baccaratvip/launch",
         "name": "Baccarat Vip",
         "gameID": "490",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/baccarat_vip.jpg",
+        "png": "//www.empiregamings.com/img/games/baccaratvip.jpg",
         "type": "table"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/baccaratmini/launch",
         "name": "Baccarat Mini",
         "gameID": "489",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/baccaratmini.jpg",
+        "png": "//www.empiregamings.com/img/games/baccaratmini.jpg",
         "type": "table"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/blackjack/launch",
         "name": "Blackjack",
         "gameID": "93",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/black_jack.jpg",
+        "png": "//www.empiregamings.com/img/games/blackjack.jpg",
         "type": "table"
     },
     {
         "launchUrl": "https://wink.empiregamings.com/wink/blackjackvip/launch",
         "name": "Blackjack Vip",
         "gameID": "487",
-        "png": "//d1t0shr650aovn.cloudfront.net/img/games/blackjackvip.jpg",
+        "png": "//www.empiregamings.com/img/games/blackjack_vip.jpg",
         "type": "table"
     }
 ]
