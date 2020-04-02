5fcaef7669ef51fa8aeb8344bab5bc7d5c8320bb
andrew.li <andrew.li@tron.network>
2020-04-02 14:03:14 +0800

    update

---
+++ tronbet_live/src/static/platiusPrd.js
@@ -0,0 +1,331 @@
+module.exports  = [
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/piratesmap/launch",
+        "name": "Pirates Map",
+        "gameID": "532",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/piratesmap.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/jackpotlab/launch",
+        "name": "Jackpot Lab",
+        "gameID": "531",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/jackpotlab.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/azteccoins/launch",
+        "name": "Aztec Coins",
+        "gameID": "529",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/azteccoins.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/chinesetigers/launch",
+        "name": "Chinese Tigers",
+        "gameID": "530",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/chinesetigers.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/rhinomania/launch",
+        "name": "Rhino Mania",
+        "gameID": "528",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/rhinomania.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/webbyheroes/launch",
+        "name": "Webby Heroes",
+        "gameID": "526",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/webbyheroes.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/luckycat/launch",
+        "name": "Lucky Cat",
+        "gameID": "527",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/luckycat.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/pharaohsempire/launch",
+        "name": "Pharaoh's Empire",
+        "gameID": "492",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/pharaohsempire.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/bisontrail/launch",
+        "name": "Bison Trail",
+        "gameID": "491",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/bisontrail.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/jadevalley/launch",
+        "name": "Jade Valley",
+        "gameID": "485",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/jadevalley.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/neonclassic/launch",
+        "name": "Neon Classic",
+        "gameID": "488",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/neonclassic.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/powerofposeidon/launch",
+        "name": "Power of Poseidon",
+        "gameID": "452",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/powerofposeidon.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/greatocean/launch",
+        "name": "Great Ocean",
+        "gameID": "486",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/greatocean.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/megadrago/launch",
+        "name": "Mega Drago",
+        "gameID": "450",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/megadrago.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/legendofatlantis/launch",
+        "name": "Legend of Atlantis",
+        "gameID": "442",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/atlantis.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/aztectemple/launch",
+        "name": "Aztec Temple",
+        "gameID": "448",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/aztectemple.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/tripledragon/launch",
+        "name": "Triple Dragon",
+        "gameID": "444",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/tripledragon.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/bookofegypt/launch",
+        "name": "Book of Egypt",
+        "gameID": "465",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/bookofegypt.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/monkeysjourney/launch",
+        "name": "Monkeys Journey",
+        "gameID": "480",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/monkeysjourney.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/sakurawind/launch",
+        "name": "Sakura Wind",
+        "gameID": "469",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/sakurawind.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/luckymoney/launch",
+        "name": "Lucky Money",
+        "gameID": "475",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/luckymoney.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/loveis/launch",
+        "name": "Love is",
+        "gameID": "476",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/loveis.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/cinderella/launch",
+        "name": "Cinderella",
+        "gameID": "477",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/cinderella.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/crystalsevens/launch",
+        "name": "Crystal Sevens",
+        "gameID": "425",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/crystalsevens.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/magicalmirror/launch",
+        "name": "Magical Mirror",
+        "gameID": "446",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/magicalmirror.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/powerofgods/launch",
+        "name": "Power of Gods",
+        "gameID": "483",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/powerofgods.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/crazyjelly/launch",
+        "name": "Crazy Jelly",
+        "gameID": "443",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/crazyjelly.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/luckydolphin/launch",
+        "name": "Lucky Dolphin",
+        "gameID": "427",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/luckydolphin.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/richywitchy/launch",
+        "name": "Richy Witchy",
+        "gameID": "429",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/richywitchy.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/magicalwolf/launch",
+        "name": "Magical Wolf",
+        "gameID": "394",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/magicalwolf.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/mistressofamazon/launch",
+        "name": "Mistress of Amazon",
+        "gameID": "393",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/mistressofamazon.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/arabiantales/launch",
+        "name": "Arabian Tales",
+        "gameID": "417",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/arabiannights.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/junglespin/launch",
+        "name": "Jungle Spin",
+        "gameID": "423",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/junglespin.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/fruitysevens/launch",
+        "name": "Fruity Sevens",
+        "gameID": "424",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/fruitysevens.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/juicyspins/launch",
+        "name": "Juicy Spins",
+        "gameID": "428",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/juicyspins.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/crocoman/launch",
+        "name": "Crocoman",
+        "gameID": "409",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/crocoman.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/safariadventures/launch",
+        "name": "Safari Adventures",
+        "gameID": "426",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/safari.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/fairyforest/launch",
+        "name": "Fairy Forest",
+        "gameID": "400",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/fairyforest.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/princessofbirds/launch",
+        "name": "Princess of Birds",
+        "gameID": "401",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/princessofbirds.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/jewelbang/launch",
+        "name": "Jewel Bang",
+        "gameID": "395",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/jewelbang.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/fieryplanet/launch",
+        "name": "Fiery Planet",
+        "gameID": "392",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/fieryplanet.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/cleosgold/launch",
+        "name": "Cleo's Gold",
+        "gameID": "386",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/cleosgold.jpg",
+        "type": "slots"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/baccarat/launch",
+        "name": "Baccarat Pro",
+        "gameID": "99",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/baccarat.jpg",
+        "type": "table"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/baccaratvip/launch",
+        "name": "Baccarat Vip",
+        "gameID": "490",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/baccarat_vip.jpg",
+        "type": "table"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/baccaratmini/launch",
+        "name": "Baccarat Mini",
+        "gameID": "489",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/baccaratmini.jpg",
+        "type": "table"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/blackjack/launch",
+        "name": "Blackjack",
+        "gameID": "93",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/black_jack.jpg",
+        "type": "table"
+    },
+    {
+        "launchUrl": "https://wink.empiregamings.com/wink/blackjackvip/launch",
+        "name": "Blackjack Vip",
+        "gameID": "487",
+        "png": "//d1t0shr650aovn.cloudfront.net/img/games/blackjackvip.jpg",
+        "type": "table"
+    }
+]
---
+++ tronbet_live/src/static/platiusTest.js
@@ -0,0 +1,331 @@
+module.exports  = [
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/piratesmap/launch",
+        "gameName": "Pirates Map",
+        "gameID": "532",
+        "png": "//platipusgaming.com/img/games/piratesmap.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/jackpotlab/launch",
+        "gameName": "Jackpot Lab",
+        "gameID": "531",
+        "png": "//platipusgaming.com/img/games/jackpotlab.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/azteccoins/launch",
+        "gameName": "Aztec Coins",
+        "gameID": "529",
+        "png": "//platipusgaming.com/img/games/azteccoins.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/chinesetigers/launch",
+        "gameName": "Chinese Tigers",
+        "gameID": "530",
+        "png": "//platipusgaming.com/img/games/chinesetigers.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/rhinomania/launch",
+        "gameName": "Rhino Mania",
+        "gameID": "528",
+        "png": "//platipusgaming.com/img/games/rhinomania.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/webbyheroes/launch",
+        "gameName": "Webby Heroes",
+        "gameID": "526",
+        "png": "//platipusgaming.com/img/games/webbyheroes.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/luckycat/launch",
+        "gameName": "Lucky Cat",
+        "gameID": "527",
+        "png": "//platipusgaming.com/img/games/luckycat.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/pharaohsempire/launch",
+        "gameName": "Pharaoh's Empire",
+        "gameID": "492",
+        "png": "//platipusgaming.com/img/games/pharaohsempire.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/bisontrail/launch",
+        "gameName": "Bison Trail",
+        "gameID": "491",
+        "png": "//platipusgaming.com/img/games/bisontrail.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/jadevalley/launch",
+        "gameName": "Jade Valley",
+        "gameID": "485",
+        "png": "//platipusgaming.com/img/games/jadevalley.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/neonclassic/launch",
+        "gameName": "Neon Classic",
+        "gameID": "488",
+        "png": "//platipusgaming.com/img/games/neonclassic.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/powerofposeidon/launch",
+        "gameName": "Power of Poseidon",
+        "gameID": "452",
+        "png": "//platipusgaming.com/img/games/powerofposeidon.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/greatocean/launch",
+        "gameName": "Great Ocean",
+        "gameID": "486",
+        "png": "//platipusgaming.com/img/games/greatocean.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/megadrago/launch",
+        "gameName": "Mega Drago",
+        "gameID": "450",
+        "png": "//platipusgaming.com/img/games/megadrago.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/legendofatlantis/launch",
+        "gameName": "Legend of Atlantis",
+        "gameID": "442",
+        "png": "//platipusgaming.com/img/games/atlantis.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/aztectemple/launch",
+        "gameName": "Aztec Temple",
+        "gameID": "448",
+        "png": "//platipusgaming.com/img/games/aztectemple.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/tripledragon/launch",
+        "gameName": "Triple Dragon",
+        "gameID": "444",
+        "png": "//platipusgaming.com/img/games/tripledragon.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/bookofegypt/launch",
+        "gameName": "Book of Egypt",
+        "gameID": "465",
+        "png": "//platipusgaming.com/img/games/bookofegypt.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/monkeysjourney/launch",
+        "gameName": "Monkeys Journey",
+        "gameID": "480",
+        "png": "//platipusgaming.com/img/games/monkeysjourney.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/sakurawind/launch",
+        "gameName": "Sakura Wind",
+        "gameID": "469",
+        "png": "//platipusgaming.com/img/games/sakurawind.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/luckymoney/launch",
+        "gameName": "Lucky Money",
+        "gameID": "475",
+        "png": "//platipusgaming.com/img/games/luckymoney.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/loveis/launch",
+        "gameName": "Love is",
+        "gameID": "476",
+        "png": "//platipusgaming.com/img/games/loveis.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/cinderella/launch",
+        "gameName": "Cinderella",
+        "gameID": "477",
+        "png": "//platipusgaming.com/img/games/cinderella.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/crystalsevens/launch",
+        "gameName": "Crystal Sevens",
+        "gameID": "425",
+        "png": "//platipusgaming.com/img/games/crystalsevens.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/magicalmirror/launch",
+        "gameName": "Magical Mirror",
+        "gameID": "446",
+        "png": "//platipusgaming.com/img/games/magicalmirror.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/powerofgods/launch",
+        "gameName": "Power of Gods",
+        "gameID": "483",
+        "png": "//platipusgaming.com/img/games/powerofgods.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/crazyjelly/launch",
+        "gameName": "Crazy Jelly",
+        "gameID": "443",
+        "png": "//platipusgaming.com/img/games/crazyjelly.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/luckydolphin/launch",
+        "gameName": "Lucky Dolphin",
+        "gameID": "427",
+        "png": "//platipusgaming.com/img/games/luckydolphin.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/richywitchy/launch",
+        "gameName": "Richy Witchy",
+        "gameID": "429",
+        "png": "//platipusgaming.com/img/games/richywitchy.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/magicalwolf/launch",
+        "gameName": "Magical Wolf",
+        "gameID": "394",
+        "png": "//platipusgaming.com/img/games/magicalwolf.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/mistressofamazon/launch",
+        "gameName": "Mistress of Amazon",
+        "gameID": "393",
+        "png": "//platipusgaming.com/img/games/mistressofamazon.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/arabiantales/launch",
+        "gameName": "Arabian Tales",
+        "gameID": "417",
+        "png": "//platipusgaming.com/img/games/arabiannights.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/junglespin/launch",
+        "gameName": "Jungle Spin",
+        "gameID": "423",
+        "png": "//platipusgaming.com/img/games/junglespin.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/fruitysevens/launch",
+        "gameName": "Fruity Sevens",
+        "gameID": "424",
+        "png": "//platipusgaming.com/img/games/fruitysevens.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/juicyspins/launch",
+        "gameName": "Juicy Spins",
+        "gameID": "428",
+        "png": "//platipusgaming.com/img/games/juicyspins.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/crocoman/launch",
+        "gameName": "Crocoman",
+        "gameID": "409",
+        "png": "//platipusgaming.com/img/games/crocoman.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/safariadventures/launch",
+        "gameName": "Safari Adventures",
+        "gameID": "426",
+        "png": "//platipusgaming.com/img/games/safari.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/fairyforest/launch",
+        "gameName": "Fairy Forest",
+        "gameID": "400",
+        "png": "//platipusgaming.com/img/games/fairyforest.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/princessofbirds/launch",
+        "gameName": "Princess of Birds",
+        "gameID": "401",
+        "png": "//platipusgaming.com/img/games/princessofbirds.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/jewelbang/launch",
+        "gameName": "Jewel Bang",
+        "gameID": "395",
+        "png": "//platipusgaming.com/img/games/jewelband.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/fieryplanet/launch",
+        "gameName": "Fiery Planet",
+        "gameID": "392",
+        "png": "//platipusgaming.com/img/games/fieryplanet.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/cleosgold/launch",
+        "gameName": "Cleo's Gold",
+        "gameID": "386",
+        "png": "//platipusgaming.com/img/games/cleosgold.jpg",
+        "category": "slots"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/baccarat/launch",
+        "gameName": "Baccarat Pro",
+        "gameID": "99",
+        "png": "//platipusgaming.com/img/games/baccarat.jpg",
+        "category": "table"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/baccaratvip/launch",
+        "gameName": "Baccarat Vip",
+        "gameID": "490",
+        "png": "//platipusgaming.com/img/games/baccaratvip.jpg",
+        "category": "table"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/baccaratmini/launch",
+        "gameName": "Baccarat Mini",
+        "gameID": "489",
+        "png": "//platipusgaming.com/img/games/baccaratmini.jpg",
+        "category": "table"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/blackjack/launch",
+        "gameName": "Blackjack",
+        "gameID": "93",
+        "png": "//platipusgaming.com/img/games/black_jack.jpg",
+        "category": "table"
+    },
+    {
+        "launchUrl": "https://wbg.platipusgaming.com/wink/blackjackvip/launch",
+        "gameName": "Blackjack Vip",
+        "gameID": "487",
+        "png": "//platipusgaming.com/img/games/blackjack_vip.jpg",
+        "category": "table"
+    }
+]
