e2f052ee729170063bc7d91109b792f5f2c1c5c6
andrew.li <andrew.li@tron.network>
2020-04-09 18:05:22 +0800

    update

--- tronbet_backend02/src/model/dice/diceQuery.js
+++ tronbet_backend02/src/model/dice/diceQuery.js
@@ -3,24 +3,25 @@ const db = require("../../utils/readDbUtil")
 const schedule = require('node-schedule');
 
 
-
 const queryDice = async function (params) {
-    let {startDate, endDate, addr,page,pageNum} = params
-    const start= newUtcTime(startDate).getTime()
-    const end= newUtcTime(endDate).getTime()
+    let {startDate, endDate, addr, page, pageNum} = params
+    const start = newUtcTime(startDate).getTime()
+    const end = newUtcTime(endDate).getTime()
     const limit = Number(pageNum)
     const offset = (Number(page) - 1) * limit
 //direction | number | roll
     let sql = `
           SELECT
                     from_unixtime( dice_user_order.ts / 1000,'%Y-%m-%d') as day,
-                    dice_user_order.addr,
+                    tron_bet_admin.dice_user_order.addr,
+                    tron_bet_wzc.dice_events_v3.order_id,
                     tron_bet_wzc.dice_events_v3.direction,
                     tron_bet_wzc.dice_events_v3.number,
                     tron_bet_wzc.dice_events_v3.roll,
                     tron_bet_wzc.dice_events_v3.amount_sun / 1000000 as amount ,
                     tron_bet_wzc.dice_events_v3.payout_sun / 1000000 as win,
-                    dice_user_order.tx_id
+                    tron_bet_admin.dice_user_order.tx_id,
+                    tron_bet_wzc.dice_events_v3.tx_id as result_tx_id
                   FROM
                     tron_bet_admin.dice_user_order
                   LEFT JOIN
@@ -34,11 +35,11 @@ const queryDice = async function (params) {
                     dice_user_order.ts DESC
     `
     let sqlC = `select count(1) as count from (${sql}) as g`
-    const crs = await raw(sqlC, [start,end,addr])
+    const crs = await raw(sqlC, [start, end, addr])
     const count = crs[0].count || 0
     //
-    sql +=  ' limit ?,?'
-    const p = [start,end,addr,offset,limit]
+    sql += ' limit ?,?'
+    const p = [start, end, addr, offset, limit]
     let rsData = await raw(sql, p)
     const rs = {
         count: count,
@@ -48,37 +49,41 @@ const queryDice = async function (params) {
 }
 
 
-
 const queryMoon = async function (params) {
-    let {startDate, endDate, addr,page,pageNum} = params
-    const start= newUtcTime(startDate).getTime()
-    const end= newUtcTime(endDate).getTime()
+    let {startDate, endDate, addr, page, pageNum} = params
+    const start = newUtcTime(startDate).getTime()
+    const end = newUtcTime(endDate).getTime()
     const limit = Number(pageNum)
     const offset = (Number(page) - 1) * limit
     let sql = `
             SELECT
-            from_unixtime(ts / 1000,'%Y-%m-%d %H:%i:%S') as day,
-            addr,
-            amount / 1000000 as amount,
-            win / 1000000  as win,
-            crashAt,
-            autoAt,
-            escapeAt as userCrashAt,
-            tx_id
+            from_unixtime(a.ts / 1000,'%Y-%m-%d %H:%i:%S') as day,
+            a.addr,
+            a.amount / 1000000 as amount,
+            a.win / 1000000  as win,
+            a.crashAt,
+            a.autoAt,
+            a.escapeAt as userCrashAt,
+            a.tx_id,
+            a.round,
+            b.tx_id as round_tx_id,
+            b.crashAt as round_crashAt
         FROM
-            tron_bet_admin.moon_user_order
+            tron_bet_admin.moon_user_order  as a
+            left join moon_round_info as b
+            on a.round = b.round
         WHERE
-            ts >= ?
-            AND ts < ?
-            And addr = ?
-            order by ts desc
+            a.ts >= ?
+            AND a.ts < ?
+            And a.addr = ?
+            order by a.ts desc
     `
     let sqlC = `select count(1) as count from (${sql}) as g`
-    const crs = await raw(sqlC, [start,end,addr])
+    const crs = await raw(sqlC, [start, end, addr])
     const count = crs[0].count || 0
     //
-    sql +=  ' limit ?,?'
-    const p = [start,end,addr,offset,limit]
+    sql += ' limit ?,?'
+    const p = [start, end, addr, offset, limit]
     let rsData = await raw(sql, p)
     const rs = {
         count: count,
@@ -88,15 +93,14 @@ const queryMoon = async function (params) {
 }
 
 
-
-
 const queryRing = async function (params) {
-    let {startDate, endDate, addr,page,pageNum,result_hash} = params
-    const start= newUtcTime(startDate).getTime()
-    const end= newUtcTime(endDate).getTime()
+    let {startDate, endDate, addr, page, pageNum, result_hash} = params
+    const start = newUtcTime(startDate).getTime()
+    const end = newUtcTime(endDate).getTime()
     const limit = Number(pageNum)
     const offset = (Number(page) - 1) * limit
-    let sql = `
+    if (result_hash !== undefined && result_hash !== '') {
+        let sql = `
             SELECT
             round,
             from_unixtime(ts / 1000,'%Y-%m-%d %H:%i:%S') as day,
@@ -109,49 +113,46 @@ const queryRing = async function (params) {
             result_hash
         FROM
             tron_bet_admin.wheel_user_order
-        WHERE
+        WHERE  result_hash = ? 
     `
-    if(result_hash !== undefined && result_hash !== ''){
-        sql += ' result_hash = ? '
-        const rs = await raw(sql,[result_hash])
+        const rs = await raw(sql, [result_hash])
         return rs
-    }else {
-        sql += ` ts >= ?
+    } else {
+        let sql = `
+            SELECT
+            a.round,
+            from_unixtime(a.ts / 1000,'%Y-%m-%d %H:%i:%S') as day,
+            a.addr,
+            a.amount / 1000000 as amount,
+            a.win / 1000000  as win,
+            a.roll as real_number,
+            a.number as user_number,
+            a.tx_id,
+            a.result_hash,
+            b.number as round_number,
+            b.tx_id as round_tx_id
+        FROM
+            tron_bet_admin.wheel_user_order as a
+            left join tron_bet_admin.wheel_round_info as b
+            on a.round = b.round
+        WHERE
+            ts >= ?
             AND ts < ?
             And addr = ?
-                order by ts desc`
+            order by ts desc `
+        let sqlC = `select count(1) as count from (${sql}) as g`
+        const crs = await raw(sqlC, [start, end, addr])
+        const count = crs[0].count || 0
+        //
+        sql += ' limit ?,?'
+        const p = [start, end, addr, offset, limit]
+        let rsData = await raw(sql, p)
+        const rs = {
+            count: count,
+            rows: rsData
+        }
+        return rs
     }
-    let sqlC = `select count(1) as count from (${sql}) as g`
-    const crs = await raw(sqlC, [start,end,addr])
-    const count = crs[0].count || 0
-    //
-    sql +=  ' limit ?,?'
-    const p = [start,end,addr,offset,limit]
-    let rsData = await raw(sql, p)
-    const rs = {
-        count: count,
-        rows: rsData
-    }
-    return rs
-}
-
-
-const queryRingResult = async function (params) {
-    let {round} = params
-    let sql = `
-            SELECT
-            round,
-            from_unixtime(ts / 1000,'%Y-%m-%d %H:%i:%S') as day,
-            tx_id,
-            number
-        FROM
-            tron_bet_admin.wheel_round_info
-        WHERE
-            round = ?
-    `
-    const p = [round]
-    let rsData = await raw(sql, p)
-    return rsData
 }
 
 
@@ -159,5 +160,4 @@ module.exports = {
     queryDice,
     queryMoon,
     queryRing,
-    queryRingResult,
 }
