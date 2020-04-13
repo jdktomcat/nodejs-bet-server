012550deda80b00393c756cc69b68216d55b2675
andrew.li <andrew.li@tron.network>
2020-04-13 16:48:37 +0800

    update

--- tronbet_backend02/src/model/live/queryBlackList.js
+++ tronbet_backend02/src/model/live/queryBlackList.js
@@ -9,9 +9,20 @@ const queryBlackList = async (params) => {
 
 const addBlackList = async (params) => {
     const {addr} = params
-    let sql = "insert into tron_live.live_black_list (addr,ts) values (?,?)"
-    let res = await raw(sql, [addr,Date.now()])
-    return res
+    //
+    let sql0 = "select * from tron_live.live_black_list where addr = ?"
+    let res0 = await raw(sql0, [addr])
+    if(res0.length === 0){
+        let sql = "insert into tron_live.live_black_list (addr,ts) values (?,?)"
+        await raw(sql, [addr,Date.now()])
+        return {
+            message : '新增成功'
+        }
+    }else {
+        return {
+            message : '该地址已经存在'
+        }
+    }
 }
 
 module.exports = {
