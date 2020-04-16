cae272ec939675ad50d0e6ef748201f40e4fc9ab
andrew.li <andrew.li@tron.network>
2020-04-16 18:11:33 +0800

    update

--- tronlive_integration/src/controller/controller.js
+++ tronlive_integration/src/controller/controller.js
@@ -6,6 +6,7 @@ class apiCall {
         const params = ctx.query || {}
         const t  = await service.identify(params)
         if(t.code === '2'){
+            console.log("debug-------->",ctx)
             ctx.status = 400;
             ctx.body = t
         }else {
