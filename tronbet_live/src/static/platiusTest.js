547a8a7d0194093499748cd88a1b1809a2e06866
andrew.li <andrew.li@tron.network>
2020-04-23 18:55:20 +0800

    update

--- tronbet_live/src/static/platiusTest.js
+++ tronbet_live/src/static/platiusTest.js
@@ -1,329 +1,329 @@
 module.exports  = [
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/piratesmap/launch",
-        "gameName": "Pirates Map",
+        "name": "Pirates Map",
         "gameID": "532",
         "png": "//platipusgaming.com/img/games/piratesmap.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/jackpotlab/launch",
-        "gameName": "Jackpot Lab",
+        "name": "Jackpot Lab",
         "gameID": "531",
         "png": "//platipusgaming.com/img/games/jackpotlab.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/azteccoins/launch",
-        "gameName": "Aztec Coins",
+        "name": "Aztec Coins",
         "gameID": "529",
         "png": "//platipusgaming.com/img/games/azteccoins.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/chinesetigers/launch",
-        "gameName": "Chinese Tigers",
+        "name": "Chinese Tigers",
         "gameID": "530",
         "png": "//platipusgaming.com/img/games/chinesetigers.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/rhinomania/launch",
-        "gameName": "Rhino Mania",
+        "name": "Rhino Mania",
         "gameID": "528",
         "png": "//platipusgaming.com/img/games/rhinomania.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/webbyheroes/launch",
-        "gameName": "Webby Heroes",
+        "name": "Webby Heroes",
         "gameID": "526",
         "png": "//platipusgaming.com/img/games/webbyheroes.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/luckycat/launch",
-        "gameName": "Lucky Cat",
+        "name": "Lucky Cat",
         "gameID": "527",
         "png": "//platipusgaming.com/img/games/luckycat.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/pharaohsempire/launch",
-        "gameName": "Pharaoh's Empire",
+        "name": "Pharaoh's Empire",
         "gameID": "492",
         "png": "//platipusgaming.com/img/games/pharaohsempire.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/bisontrail/launch",
-        "gameName": "Bison Trail",
+        "name": "Bison Trail",
         "gameID": "491",
         "png": "//platipusgaming.com/img/games/bisontrail.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/jadevalley/launch",
-        "gameName": "Jade Valley",
+        "name": "Jade Valley",
         "gameID": "485",
         "png": "//platipusgaming.com/img/games/jadevalley.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/neonclassic/launch",
-        "gameName": "Neon Classic",
+        "name": "Neon Classic",
         "gameID": "488",
         "png": "//platipusgaming.com/img/games/neonclassic.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/powerofposeidon/launch",
-        "gameName": "Power of Poseidon",
+        "name": "Power of Poseidon",
         "gameID": "452",
         "png": "//platipusgaming.com/img/games/powerofposeidon.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/greatocean/launch",
-        "gameName": "Great Ocean",
+        "name": "Great Ocean",
         "gameID": "486",
         "png": "//platipusgaming.com/img/games/greatocean.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/megadrago/launch",
-        "gameName": "Mega Drago",
+        "name": "Mega Drago",
         "gameID": "450",
         "png": "//platipusgaming.com/img/games/megadrago.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/legendofatlantis/launch",
-        "gameName": "Legend of Atlantis",
+        "name": "Legend of Atlantis",
         "gameID": "442",
         "png": "//platipusgaming.com/img/games/atlantis.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/aztectemple/launch",
-        "gameName": "Aztec Temple",
+        "name": "Aztec Temple",
         "gameID": "448",
         "png": "//platipusgaming.com/img/games/aztectemple.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/tripledragon/launch",
-        "gameName": "Triple Dragon",
+        "name": "Triple Dragon",
         "gameID": "444",
         "png": "//platipusgaming.com/img/games/tripledragon.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/bookofegypt/launch",
-        "gameName": "Book of Egypt",
+        "name": "Book of Egypt",
         "gameID": "465",
         "png": "//platipusgaming.com/img/games/bookofegypt.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/monkeysjourney/launch",
-        "gameName": "Monkeys Journey",
+        "name": "Monkeys Journey",
         "gameID": "480",
         "png": "//platipusgaming.com/img/games/monkeysjourney.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/sakurawind/launch",
-        "gameName": "Sakura Wind",
+        "name": "Sakura Wind",
         "gameID": "469",
         "png": "//platipusgaming.com/img/games/sakurawind.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/luckymoney/launch",
-        "gameName": "Lucky Money",
+        "name": "Lucky Money",
         "gameID": "475",
         "png": "//platipusgaming.com/img/games/luckymoney.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/loveis/launch",
-        "gameName": "Love is",
+        "name": "Love is",
         "gameID": "476",
         "png": "//platipusgaming.com/img/games/loveis.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/cinderella/launch",
-        "gameName": "Cinderella",
+        "name": "Cinderella",
         "gameID": "477",
         "png": "//platipusgaming.com/img/games/cinderella.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/crystalsevens/launch",
-        "gameName": "Crystal Sevens",
+        "name": "Crystal Sevens",
         "gameID": "425",
         "png": "//platipusgaming.com/img/games/crystalsevens.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/magicalmirror/launch",
-        "gameName": "Magical Mirror",
+        "name": "Magical Mirror",
         "gameID": "446",
         "png": "//platipusgaming.com/img/games/magicalmirror.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/powerofgods/launch",
-        "gameName": "Power of Gods",
+        "name": "Power of Gods",
         "gameID": "483",
         "png": "//platipusgaming.com/img/games/powerofgods.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/crazyjelly/launch",
-        "gameName": "Crazy Jelly",
+        "name": "Crazy Jelly",
         "gameID": "443",
         "png": "//platipusgaming.com/img/games/crazyjelly.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/luckydolphin/launch",
-        "gameName": "Lucky Dolphin",
+        "name": "Lucky Dolphin",
         "gameID": "427",
         "png": "//platipusgaming.com/img/games/luckydolphin.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/richywitchy/launch",
-        "gameName": "Richy Witchy",
+        "name": "Richy Witchy",
         "gameID": "429",
         "png": "//platipusgaming.com/img/games/richywitchy.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/magicalwolf/launch",
-        "gameName": "Magical Wolf",
+        "name": "Magical Wolf",
         "gameID": "394",
         "png": "//platipusgaming.com/img/games/magicalwolf.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/mistressofamazon/launch",
-        "gameName": "Mistress of Amazon",
+        "name": "Mistress of Amazon",
         "gameID": "393",
         "png": "//platipusgaming.com/img/games/mistressofamazon.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/arabiantales/launch",
-        "gameName": "Arabian Tales",
+        "name": "Arabian Tales",
         "gameID": "417",
         "png": "//platipusgaming.com/img/games/arabiannights.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/junglespin/launch",
-        "gameName": "Jungle Spin",
+        "name": "Jungle Spin",
         "gameID": "423",
         "png": "//platipusgaming.com/img/games/junglespin.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/fruitysevens/launch",
-        "gameName": "Fruity Sevens",
+        "name": "Fruity Sevens",
         "gameID": "424",
         "png": "//platipusgaming.com/img/games/fruitysevens.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/juicyspins/launch",
-        "gameName": "Juicy Spins",
+        "name": "Juicy Spins",
         "gameID": "428",
         "png": "//platipusgaming.com/img/games/juicyspins.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/crocoman/launch",
-        "gameName": "Crocoman",
+        "name": "Crocoman",
         "gameID": "409",
         "png": "//platipusgaming.com/img/games/crocoman.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/safariadventures/launch",
-        "gameName": "Safari Adventures",
+        "name": "Safari Adventures",
         "gameID": "426",
         "png": "//platipusgaming.com/img/games/safari.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/fairyforest/launch",
-        "gameName": "Fairy Forest",
+        "name": "Fairy Forest",
         "gameID": "400",
         "png": "//platipusgaming.com/img/games/fairyforest.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/princessofbirds/launch",
-        "gameName": "Princess of Birds",
+        "name": "Princess of Birds",
         "gameID": "401",
         "png": "//platipusgaming.com/img/games/princessofbirds.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/jewelbang/launch",
-        "gameName": "Jewel Bang",
+        "name": "Jewel Bang",
         "gameID": "395",
         "png": "//platipusgaming.com/img/games/jewelband.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/fieryplanet/launch",
-        "gameName": "Fiery Planet",
+        "name": "Fiery Planet",
         "gameID": "392",
         "png": "//platipusgaming.com/img/games/fieryplanet.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/cleosgold/launch",
-        "gameName": "Cleo's Gold",
+        "name": "Cleo's Gold",
         "gameID": "386",
         "png": "//platipusgaming.com/img/games/cleosgold.jpg",
         "category": "slots"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/baccarat/launch",
-        "gameName": "Baccarat Pro",
+        "name": "Baccarat Pro",
         "gameID": "99",
         "png": "//platipusgaming.com/img/games/baccarat.jpg",
         "category": "table"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/baccaratvip/launch",
-        "gameName": "Baccarat Vip",
+        "name": "Baccarat Vip",
         "gameID": "490",
         "png": "//platipusgaming.com/img/games/baccaratvip.jpg",
         "category": "table"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/baccaratmini/launch",
-        "gameName": "Baccarat Mini",
+        "name": "Baccarat Mini",
         "gameID": "489",
         "png": "//platipusgaming.com/img/games/baccaratmini.jpg",
         "category": "table"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/blackjack/launch",
-        "gameName": "Blackjack",
+        "name": "Blackjack",
         "gameID": "93",
         "png": "//platipusgaming.com/img/games/black_jack.jpg",
         "category": "table"
     },
     {
         "launchUrl": "https://wbg.platipusgaming.com/wink/blackjackvip/launch",
-        "gameName": "Blackjack Vip",
+        "name": "Blackjack Vip",
         "gameID": "487",
         "png": "//platipusgaming.com/img/games/blackjack_vip.jpg",
         "category": "table"
