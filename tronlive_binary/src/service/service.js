54509ab655701a573666dcfd3f2a23e40edbe899
andrew.li <andrew.li@tron.network>
2020-05-13 15:17:05 +0800

    update

--- tronlive_binary/src/service/service.js
+++ tronlive_binary/src/service/service.js
@@ -97,9 +97,9 @@ class Service {
             return t
         }
         const user = tokenInfo.user || ''
-        console.log("user is ",user)
         const whiteList = ['TXdWwzoq74BKUQx4JeEYnUs41EdGpyZKbP','TUMqj1BGbqTcp8j9gDLQWDWYV1cKXgCmf9']
-        if(whiteList.includes(user.trim())){
+        console.log("user is ",user,!whiteList.includes(user.trim()))
+        if(!whiteList.includes(user.trim())){
             return this.error("user not allowed!")
         }
         return t
