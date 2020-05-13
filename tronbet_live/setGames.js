6eff680b81b75759ec0ba473082fee5f5895854c
andrew.li <andrew.li@tron.network>
2020-05-13 18:52:25 +0800

    update

--- tronbet_live/setGames.js
+++ tronbet_live/setGames.js
@@ -65,7 +65,7 @@ const main = async function () {
         const roundInfo = await raw(roundSql, [round])
         console.log(round, addr,"---->", JSON.stringify(roundInfo[0]))
         let initWheelInfoSql = "update wheel_user_order set roll = ?, luckyNum = ?,result_hash = ?,salt = ?, mentor = '', win = 0, referralAmount = 0 where round = ? and win is null and roll is null"
-        await raw(initWheelInfoSql, [roundInfo[0].roll, roundInfo[0].luckyNum, roundInfo[0].hash, roundInfo[0].salt, infos.round])
+        await raw(initWheelInfoSql, [roundInfo[0].roll, roundInfo[0].luckyNum, roundInfo[0].hash, roundInfo[0].salt, round])
 
     }
 }
