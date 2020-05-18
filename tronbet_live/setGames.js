b094a0975f4fb6101f3382ddf3ca9f3826edcfd9
gordon <gordon@gordondeMacBook-Pro.local>
2020-05-18 14:49:01 +0800

    update burnWin to db

--- tronbet_live/setGames.js
+++ tronbet_live/setGames.js
@@ -1,38 +1,45 @@
-let prdCfg = {};
-try {
-  prdCfg = require('/data/tronbet_config/config');
-  // prdCfg = require("/data/tronbet_config/config_test");
-} catch (error) {
-  console.log("using app config");
+const db = require("./src/utils/dbUtil");
+
+async function insertDB(info) {
+  let sql =
+    "insert into tron_bet_admin.ante_burnt_log(types, call_addr, amount, ts, tx_id) values (?, ?, ?, ?, ?) ON DUPLICATE KEY update tx_id = ?";
+
+  let res = await db.exec(sql, [
+    info.type,
+    info.addr,
+    info.amount,
+    info.ts,
+    info.tx,
+    info.tx
+  ]);
+  console.log("insertDB res", res);
 }
 
-const TronWeb = require("tronweb");
-const HttpProvider = TronWeb.providers.HttpProvider;
-const fullNode = new HttpProvider(prdCfg.master_full);
-const solidityNode = new HttpProvider(prdCfg.master_solidity);
-const eventServer = prdCfg.master_event;
-const privateKey = prdCfg.operatorDice_pk;
+async function main() {
+  let info = [];
+  let obj1 = {};
+  //奖池燃烧
+  obj1.type = 2;
+  obj1.addr = "THtbMw6byXuiFhsRv1o1BQRtzvube9X1jx";
+  obj1.amount = 43060975336900;
+  obj1.ts = 1589783955000;
+  obj1.tx = "37b9af6fa090c6258a77fe162b3e0aec5b0315fcfe259b320f1138c0f9404f81";
+  info.push(obj1);
 
-let tronWeb = new TronWeb(fullNode, solidityNode, eventServer, privateKey);
+  let obj3 = {};
+  //回购燃烧
+  obj3.type = 1;
+  obj3.addr = "THNpF5h4isLgXe7rtw6833nSgTqhfuVJLN";
+  obj3.amount = 376007484281000;
+  obj3.ts = 1586846238000;
+  obj3.tx = "6894c11fd6c6424a04991f06565941028f68b1a5fd3a971a17987867e96967fe";
+  //info.push(obj3);
 
-const contract_address = prdCfg.contract.TronBetPool20; //合约地址
+  for (let item of info) {
+    await insertDB(item);
+  }
 
-//获取合约
-async function getContractInstance() {
-  let contractInstance = await tronWeb.contract().at(contract_address);
-  return contractInstance;
+  process.exit(0);
 }
 
-async function burn() {
-  let contractInstance = await getContractInstance();
-  const transactionID = await contractInstance
-    .burnWin()
-    .send()
-    .catch(error => {
-      console.log(error);
-    });
-  console.log(transactionID);
-  return transactionID;
-}
-
-burn();
+main();
