e641134b102d85380518ddaa3c38a5b5666cb328
andrew.li <andrew.li@tron.network>
2020-05-25 14:16:29 +0800

    update

--- tronbet_backend02/src/dailySchedule/diceRankReload.js
+++ tronbet_backend02/src/dailySchedule/diceRankReload.js
@@ -1,26 +1,26 @@
 const scanDiceRestart = function () {
-    console.log("----reScanDice---->", new Date());
-    const cmd = 'pm2 reload tronbet_scan_dice'
+    console.log("----diceRankReload---->", new Date());
+    const cmd = 'pm2 reload tronbet_rank'
     try{
         const child_process = require("child_process")
         const a = child_process.execSync(cmd).toString()
-        console.log("scanDiceRestart_out: \n",a)
+        console.log("diceRankReload_out: \n",a)
         return a
     }catch (e) {
-        console.log("scanDiceRestart_error: \n",e)
+        console.log("diceRankReload_error: \n",e)
         return e.toString()
     }
 }
 
-const diceScan = function () {
+const rankSchedule = function () {
     // 每个小时30分的时候重启dice扫描
     // '42 * * * *'
     const schedule = require('node-schedule');
-    const a1 = schedule.scheduleJob('25 * * * *', function () {
-        console.log(new Date(),"diceScanSchedule")
+    const a1 = schedule.scheduleJob('20 * * * *', function () {
+        console.log(new Date(),"diceRankReload")
         scanDiceRestart()
     })
 
 }
 
-module.exports = diceScan
+module.exports = rankSchedule
