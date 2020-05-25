e9406d474454e2f049f54157ba775f956a06e76f
andrew.li <andrew.li@tron.network>
2020-05-25 18:38:59 +0800

    update

--- tronbet_live_wallet/service/balance_service.js
+++ tronbet_live_wallet/service/balance_service.js
@@ -1,6 +1,6 @@
 const model = require("./../model/model")
 
-const parseParams = function (params) {
+const parseParams = async function (params) {
     console.log("request params: ", params)
     let k = {}
     Object.keys(params).forEach(e => {
@@ -9,38 +9,34 @@ const parseParams = function (params) {
             k[e] = v
         }
     })
+    if (!['TRX','USDT'].includes(params.currency)) {
+        throw new Error("currency error!")
+    }
     const amount = params.amount || 0
     if (amount < 0) {
         throw new Error("amount error!")
     }
     // uid 还是addr
     const uid = params.uid || ''
-    const addr = params.addr || ''
-    console.log("uid is ",uid)
-    console.log("addr is ",addr)
-    console.log("uid === '' && addr === '' ",uid === '' && addr === '')
+    let addr = params.addr || ''
     if (uid === '' && addr === '') {
-        // request params:  { uid: 33749, currency: 'TRX', amount: 500000 }
         throw new Error("uid and addr is empty!")
     }
-    //
-    if (uid === '') {
-        // 使用addr
-        const t = {
-            addr: params.addr,
-            currency: params.currency,
-            amount: amount,
-        }
-        return t
-    } else if (addr === '') {
-        // 使用uid
-        const t = {
-            uid: params.uid,
-            currency: params.currency,
-            amount: amount,
-        }
-        return t
+    if (addr === '') {
+        const balanceInfo = await model.getOne({
+            where: {
+                uid: params.uid,
+                currency: params.currency,
+            }
+        })
+        addr = balanceInfo.addr
     }
+    const t = {
+        addr: addr,
+        currency: params.currency,
+        amount: amount,
+    }
+    return t
 }
 
 class opBalance {
@@ -54,13 +50,13 @@ class opBalance {
     }
 
     static async addBalance(rawParams) {
-        const params = parseParams(rawParams)
+        const params = await parseParams(rawParams)
         const msg = await model.addBalance(params)
         return msg
     }
 
     static async decreaseBalance(rawParams) {
-        const params = parseParams(rawParams)
+        const params = await parseParams(rawParams)
         const msg = await model.decreaseBalance(params)
         return msg
     }
