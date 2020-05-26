bd353efe5cf4beb986e4b7bffb53a6073c0c2a89
gordon <gordon@gordondeMacBook-Pro.local>
2020-05-26 11:49:21 +0800

    update user balance

--- tronbet_live/setGames.js
+++ tronbet_live/setGames.js
@@ -1,45 +1,33 @@
 const db = require("./src/utils/dbUtil");
 
-async function insertDB(info) {
-  let sql =
-    "insert into tron_bet_admin.ante_burnt_log(types, call_addr, amount, ts, tx_id) values (?, ?, ?, ?, ?) ON DUPLICATE KEY update tx_id = ?";
+const update2020526 = async function () {
+    const addr = [
+            'TSDNKq7X9E3DJP9jFyPwoVUPA63bK83pdK'
+        ]
+    for(let e of addr){
+        const sql1 = `select uid,currency,addr,balance / 1000000 as balance  from tron_live.live_balance where addr = ? and currency = 'TRX'`
+        const a1 = await db.exec(sql1,[e])
+        console.log("before is ",a1)
+        const updateSql = `update tron_live.live_balance set balance = 0 where addr = ? and currency = 'TRX' `
+        await db.exec(updateSql,[e])
+    }
+    console.log("\n\n===>\n")
+    for(let e of addr){
+        const sql1 = `select  uid,currency,addr,balance / 1000000 as balance   from tron_live.live_balance where addr = ? and currency = 'TRX'`
+        const a1 = await db.exec(sql1,[e])
+        console.log("after is ",a1)
+    }
 
-  let res = await db.exec(sql, [
-    info.type,
-    info.addr,
-    info.amount,
-    info.ts,
-    info.tx,
-    info.tx
-  ]);
-  console.log("insertDB res", res);
 }
 
-async function main() {
-  let info = [];
-  let obj1 = {};
-  //奖池燃烧
-  obj1.type = 2;
-  obj1.addr = "THtbMw6byXuiFhsRv1o1BQRtzvube9X1jx";
-  obj1.amount = 43060975336900;
-  obj1.ts = 1589783955000;
-  obj1.tx = "37b9af6fa090c6258a77fe162b3e0aec5b0315fcfe259b320f1138c0f9404f81";
-  info.push(obj1);
-
-  let obj3 = {};
-  //回购燃烧
-  obj3.type = 1;
-  obj3.addr = "TW8UBosxaTYKAS22WywSVnyjjJr4iEvjNr";
-  obj3.amount = 408516594925900;
-  obj3.ts = 1589880267000;
-  obj3.tx = "f98f26201eef86c9aad885e4d4e6b9aba5ba814e5df020eda1855e4546bc210a";
-  info.push(obj3);
-
-  for (let item of info) {
-    await insertDB(item);
-  }
-
-  process.exit(0);
+const main = async function(){
+    await update2020526()
 }
 
-main();
+main().then(() => {
+    console.log("end!")
+    process.exit(0)
+}).catch(e => {
+    console.log('error is : ', e)
+    process.exit(1)
+})
