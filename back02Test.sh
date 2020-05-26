1c1ad4796c822accc3f95355918c06464e550911
andrew.li <andrew.li@tron.network>
2020-05-26 14:55:26 +0800

    update

--- back02Test.sh
+++ back02Test.sh
@@ -47,3 +47,4 @@ cd tronswagger_hub && pm2 start npm --name tronswagger_hub -- run test && cd ..
 
 cd tronlive_platius && pm2 start npm --name tronlive_platius -- run test && cd ..
 cd tronlive_binary && pm2 start npm --name tronlive_binary -- run test && cd ..
+cd tronbet_live_wallet && pm2 start npm --name tronbet_live_wallet -- run test && cd ..
