7ecc938566bc4ebfcbd3d329dadd97ac2a41af8e
gordon <gordon@gordondeMacBook-Pro.local>
2020-05-28 16:37:05 +0800

    update user balance set user balance=0 where user_balance_audit is not normal

--- tronbet_live/setGames.js
+++ tronbet_live/setGames.js
@@ -1,59 +1,17 @@
 const db = require("./src/utils/dbUtil");
 
-const dropTable = async function () {
-	const sql = "DROP TABLE live_balance_audit";
-	const rs = await db.exec(sql,[])
-}
-
-const createTable = async function () {
-	const sql = `CREATE TABLE live_balance_audit (
-  id bigint(20) NOT NULL AUTO_INCREMENT,
-  addr varchar(64) NOT NULL,
-  live_balance bigint(20) unsigned NOT NULL,
-  calc_balance bigint(20) NOT NULL,
-  flag varchar(32) NOT NULL DEFAULT 'normal', -- normal, malicious
-  live_balance_1 bigint(20) unsigned DEFAULT 0,
-  live_balance_2 bigint(20) unsigned DEFAULT 0,
-  live_balance_3 bigint(20) unsigned DEFAULT 0,
-  live_balance_4 bigint(20) unsigned DEFAULT 0,
-  live_balance_5 bigint(20) unsigned DEFAULT 0,
-  live_balance_6 bigint(20) unsigned DEFAULT 0,
-  live_balance_7 bigint(20) unsigned DEFAULT 0,
-  live_balance_8 bigint(20) unsigned DEFAULT 0,
-  live_balance_9 bigint(20) unsigned DEFAULT 0,
-  live_balance_10 bigint(20) unsigned DEFAULT 0,
-  live_balance_11 bigint(20) unsigned DEFAULT 0,
-  live_balance_12 bigint(20) unsigned DEFAULT 0,
-  live_balance_13 bigint(20) unsigned DEFAULT 0,
-  live_balance_14 bigint(20) unsigned DEFAULT 0,
-  live_balance_15 bigint(20) unsigned DEFAULT 0,
-  live_balance_16 bigint(20) unsigned DEFAULT 0,
-  live_balance_17 bigint(20) unsigned DEFAULT 0,
-  live_balance_18 bigint(20) unsigned DEFAULT 0,
-  live_balance_19 bigint(20) unsigned DEFAULT 0,
-  live_balance_20 bigint(20) unsigned DEFAULT 0,
-  live_balance_21 bigint(20) unsigned DEFAULT 0,
-  live_balance_22 bigint(20) unsigned DEFAULT 0,
-  live_balance_23 bigint(20) unsigned DEFAULT 0,
-  live_balance_24 bigint(20) unsigned DEFAULT 0,
-  live_balance_25 bigint(20) unsigned DEFAULT 0,
-  live_balance_26 bigint(20) unsigned DEFAULT 0,
-  live_balance_27 bigint(20) unsigned DEFAULT 0,
-  live_balance_28 bigint(20) unsigned DEFAULT 0,
-  live_balance_29 bigint(20) unsigned DEFAULT 0,
-  live_balance_30 bigint(20) unsigned DEFAULT 0,
-  live_balance_31 bigint(20) unsigned DEFAULT 0,
-  create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  last_modify_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (id),
-  UNIQUE KEY live_balance_audit_addr_index (addr) 
-) ENGINE=InnoDB DEFAULT CHARSET=utf8`
-	const rs = await db.exec(sql,[])
-}
-
 const main = async function(){
-    await dropTable()
-    await createTable()
+    let checkSql=`select * from live_balance where addr in(select addr from live_balance_audit where flag!='normal')`;
+    let beforeReset=await db.query(checkSql,[]);
+    console.log("before reset")
+    console.log(beforeReset)
+    let resetSql=`update live_balance set balance=0  where addr in(select addr from live_balance_audit where flag!='normal') and addr!='TMtb6tEzPWFkd1ucT4LQabp3GK17tpK3TJ'`;
+    let result =await db.exec(resetSql,[]);
+    console.log("reset Result");
+    console.log(result);
+    let afterReset=await db.query(checkSql,[]);
+    console.log("after reset")
+    console.log(afterReset)
 }
 
 main().then(() => {
@@ -63,3 +21,4 @@ main().then(() => {
     console.log('error is : ', e)
     process.exit(1)
 })
+
