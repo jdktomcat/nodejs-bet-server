6f0f5218233999d172ee75488666cdec5645f979
sid.tang <jdktomcat@163.com>
2020-06-03 17:20:24 +0800

    合并master代码

--- tronbet_live/setGames.js
+++ tronbet_live/setGames.js
@@ -71,6 +71,24 @@ const addBalance = async function (addr, amount) {
     await query_balance(addr);
 }
 
+/**
+ * 创建清空账户记录表
+ */
+const createClearLogTable = async function () {
+    let sql = `
+        CREATE TABLE \`live_balance_clear_log\` (
+            \`id\` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+            \`addr\` varchar(100) NOT NULL COMMENT '账户钱包地址或邮箱地址',
+            \`clear_balance\` bigint(20) unsigned NOT NULL COMMENT '清除金额',
+            \`live_balance\` bigint(20) NOT NULL COMMENT '清除账户余额时对应的余额',
+            \`cal_balance\` bigint(20) NOT NULL COMMENT '计算出流水余额',
+            \`create_time\` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+            PRIMARY KEY (\`id\`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='清空账户日志表'
+    `;
+    let res = await db.query(sql, []);
+}
+
 addBalance('TTee3vKWqtZaafkuTEtwFd2QHwcyGkNEnj', 100 * 10000 * 1000000).then(() => {
 // main().then(() => {
     console.log("end!")
