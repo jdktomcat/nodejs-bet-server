be5b8bc925d0dbb871d413617435461ddc74fd44
sid.tang <jdktomcat@163.com>
2020-06-03 16:35:59 +0800

    账户余额异常查询接口

--- tronbet_backend02/src/model/balanceAudit.js
+++ tronbet_backend02/src/model/balanceAudit.js
@@ -17,10 +17,10 @@ class BalanceAudit {
     static async getBalanceAuditPage(addr, startTime, endTime, offset, limit) {
         let sql, params
         if (addr) {
-            sql = `select id, addr, live_balance, cal_balance, clear_amount, flag, create_time, last_modify_time from live_balance_audit where  addr = ? AND flag = 'malicious'`
+            sql = `select id, addr, live_balance, calc_balance, clear_amount, flag, create_time, last_modify_time from live_balance_audit where  addr = ? AND flag = 'malicious'`
             params = [addr]
         } else {
-            sql = `select id, addr, live_balance, cal_balance, flag, create_time, last_modify_time from 
+            sql = `select id, addr, live_balance, calc_balance, flag, create_time, last_modify_time from 
                          tron_live.live_balance_audit where create_time >= ? AND create_time < ? AND flag = 'malicious' order by create_time desc limit ?,?`
             params = [startTime, endTime, offset, limit]
         }
@@ -38,10 +38,10 @@ class BalanceAudit {
     static async getBalanceAuditList(addr, startTime, endTime) {
         let sql, params
         if (addr) {
-            sql = `select id, addr, live_balance, cal_balance, clear_amount, flag, create_time, last_modify_time from live_balance_audit where  addr = ? flag = 'malicious'`
+            sql = `select id, addr, live_balance, calc_balance, clear_amount, flag, create_time, last_modify_time from live_balance_audit where  addr = ? AND flag = 'malicious'`
             params = [addr]
         } else {
-            sql = `select id, addr, live_balance, cal_balance, clear_amount, flag, create_time, last_modify_time from 
+            sql = `select id, addr, live_balance, calc_balance, clear_amount, flag, create_time, last_modify_time from 
                          live_balance_audit where create_time >= ? AND create_time < ? AND flag = 'malicious' order by create_time desc`
             params = [
                 startTime, endTime
