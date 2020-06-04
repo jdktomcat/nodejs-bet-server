19f4b93bd425c7934bbf8472a2d369a43e6521be
declan-yang <declan.yang@tron.network>
2020-06-04 14:57:14 +0800

    misc(tronbet_live): add table live_balance_audit_all in setGames

--- tronbet_live/setGames.js
+++ tronbet_live/setGames.js
@@ -1,13 +1,13 @@
 const db = require("./src/utils/dbUtil");
 
-const query_balance = async function (addr) {
-    const sql = "select * from tron_live.live_balance where addr = ? and currency = 'TRX'"
-    const params = [addr]
-    console.log(sql, params)
-    const a = await db.exec(sql, params)
-    console.log("balance info is ", a)
-    //
-}
+// const query_balance = async function (addr) {
+//     const sql = "select * from tron_live.live_balance where addr = ? and currency = 'TRX'"
+//     const params = [addr]
+//     console.log(sql, params)
+//     const a = await db.exec(sql, params)
+//     console.log("balance info is ", a)
+//     //
+// }
 
 // const update_balance = async function (addr,win) {
 //     const update_balance_sql = "update tron_live.live_balance set balance = balance + ? where addr = ? and currency = 'TRX'"
@@ -59,19 +59,125 @@ const query_balance = async function (addr) {
 //     }
 // }
 
-const addBalance = async function (addr, amount) {
-    await query_balance(addr);
+// const addBalance = async function (addr, amount) {
+//     await query_balance(addr);
 
-    const sql = "update tron_live.live_balance set balance = balance + ?  where addr = ? and currency = 'TRX'";
-    const params = [amount, addr]
-    console.log("before addBalance: addr: %s, amount: %d", addr, amount);
-    const a = await db.exec(sql, params)
-    console.log("after addBalance: addr: %s, amount: %d", addr, amount);
+//     const sql = "update tron_live.live_balance set balance = balance + ?  where addr = ? and currency = 'TRX'";
+//     const params = [amount, addr]
+//     console.log("before addBalance: addr: %s, amount: %d", addr, amount);
+//     const a = await db.exec(sql, params)
+//     console.log("after addBalance: addr: %s, amount: %d", addr, amount);
 
-    await query_balance(addr);
+//     await query_balance(addr);
+// }
+
+const createTable = async function () {
+    const sql = `
+        CREATE TABLE IF NOT EXISTS live_balance_audit_all (
+          id bigint(20) NOT NULL AUTO_INCREMENT,
+          addr varchar(64) NOT NULL,
+          live_balance bigint(20) NOT NULL,
+          calc_balance bigint(20) NOT NULL,
+          diff bigint(20) NOT NULL,
+          PRIMARY KEY (id),
+          UNIQUE KEY live_balance_audit_addr_all_index (addr)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8`;
+    const a = await db.exec(sql, [])
+    console.log("after createTable");
 }
 
-addBalance('TTee3vKWqtZaafkuTEtwFd2QHwcyGkNEnj', 100 * 10000 * 1000000).then(() => {
+let batchSize=300;
+let startUid=43073;
+
+const queryLiveBalanceAndCalcBalance = async function (addresses) {
+    let sql = `
+        select addr, sum(info.live_balance) as liveBalance,
+            sum(info.deposit) - sum(info.withdraw) - sum(info.amount) + sum(info.win) as calcBalance,
+            live_balance - (sum(info.deposit) - sum(info.withdraw) - sum(info.amount) + sum(info.win)) as diff
+            from
+            (
+                select sum(balance) as live_balance, 0 as deposit, 0 as withdraw, 0 as amount, 0 as win,addr
+                    from tron_live.live_balance where addr in (?) and currency = 'trx' group by addr
+                union all
+                select 0 as live_balance, sum(amount) as deposit, 0 as withdraw, 0 as amount, 0 as win,addr
+                    from tron_live.live_cb_deposit_log where addr in (?) and currency = 'trx' group by addr
+                union all
+                select 0 as live_balance, 0 as deposit, sum(amount) as withdraw, 0 as amount, 0 as win,addr
+                    from tron_live.live_cb_withdraw_log where addr in (?) and currency = 'trx' and status = 1 group by addr
+                union all
+                select 0 as live_balance, 0 as deposit, 0 as withdraw, sum(total.amount) as amount, sum(total.win) as win,addr from
+                (
+                    select sum(em.amount) as amount, sum(em.win) as win,addr  from
+                    (
+                        select cast(sum(amount) * 1000000 as unsigned) as amount, 0 as win,addr  from tron_live.live_action_log_v2
+                            where addr in(?) and currency = 'trx' and action = 'bet' and txstatus = 1 group by addr
+                        union all
+                        select 0 as amount, cast(sum(amount) * 1000000 as unsigned) as win,addr from tron_live.live_action_log_v2
+                            where addr in(?) and currency = 'trx' and action = 'result' and txstatus = 1 group by addr
+                    ) as em group by addr
+                    union all
+                    select sum(amount) as amount, sum(win) as win,email as addr
+                        from tron_live.swagger_transaction_log where email in (?) and currency = 'trx' and status = 1 group by email
+                    union all
+                    select sum(amount) as amount, sum(win) as win,addr
+                        from tron_live.sports_transaction_log where addr in (?) and currency = 'trx' and (status = 50 or status = 51) group by addr
+                    union all
+                    select sum(amount) as amount, sum(win) as win,addr
+                        from tron_live.platipus_transaction_log where addr in (?) and currency = 'trx' and status = 2 and resultId is not null group by addr
+                    union all
+                    select sum(amount) as amount, sum(win) as win,addr
+                        from tron_live.binary_transaction_log where addr in (?) and currency = 'trx' and status = 'close' group by addr
+                )  as total group by addr
+            ) as info group by addr`
+
+    const data = await db.query(sql, [addresses, addresses, addresses, addresses, addresses, addresses, addresses, addresses, addresses])
+    return data
+}
+
+const queryAccounts =async function(){
+    let params =['TRX', startUid]
+    let sql="select * from live_balance where currency=? and uid>?"
+    let result=await db.query(sql,params);
+    return result;
+}
+
+const doBatchUpdate = async function(list){
+    for(let i=0;i<Math.floor(list.length/batchSize+1);i++){
+        let start=i*batchSize;
+        let end=(i+1)*batchSize;
+        if(end>list.length){
+            end=list.length;
+        }
+        let bl=list.slice(start,end);
+        let addresses=[];
+        bl.forEach((record)=>{
+            addresses.push(record.addr);
+        });
+        let rl=await queryLiveBalanceAndCalcBalance(addresses);
+        if (!rl || rl.length===0){
+            console.log("queryLiveBalanceAndCalcBalance error")
+            return
+        }
+        let il=[]
+        rl.forEach((record)=>{
+            il.push([record.addr,record.liveBalance,record.calcBalance,record.diff]);
+        });
+        let sql="insert into tron_live.live_balance_audit_all (addr, live_balance, calc_balance, diff) values ? on duplicate key update live_balance=values(live_balance),calc_balance=values(calc_balance),diff=values(diff)";
+        await db.query(sql, [il]);
+    }
+}
+
+const doJob=async function(){
+    await createTable();
+
+    let list= await queryAccounts();
+    if (list && list.length>0){
+        await doBatchUpdate(list)
+    }
+}
+
+doJob().then(() => {
+// addBalance('TTee3vKWqtZaafkuTEtwFd2QHwcyGkNEnj', 100 * 10000 * 1000000).then(() => {
 // main().then(() => {
     console.log("end!")
     process.exit(0)
