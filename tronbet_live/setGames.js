e3bec92fc9b7c3fb55162b8a2b144066556aa8c8
gordon <gordon@gordondeMacBook-Pro.local>
2020-06-10 14:09:52 +0800

    转账800万到老板账户

--- tronbet_live/setGames.js
+++ tronbet_live/setGames.js
@@ -3,5 +3,4 @@
  * 此脚本只引用，若调用自己脚本，先将其他人的引用注释
  *
  */
-// const test1 = require("./z_excute/update1")
-const test1 = require("./z_excute/updateDeclan")
+const test = require("./z_excute/updateGordon")
