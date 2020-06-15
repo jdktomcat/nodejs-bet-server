2b500b30d704c4aa8fbf714708d953736b297cbc
sid.tang <jdktomcat@163.com>
2020-06-15 10:35:23 +0800

    消息发送改造 2020-06-15

--- tronbet_event/src/model/activity.js
+++ tronbet_event/src/model/activity.js
@@ -14,6 +14,42 @@ async function saveUserBetLog(dataList) {
     console.log("save user bet log complete,result:" + insertResult)
 }
 
+/**
+ * 批量保存用户积分信息
+ *
+ * @param dataList 用户积分信息列表
+ * @returns {Promise<void>}
+ */
+async function saveUserIntegral(dataList) {
+    if (!dataList || dataList.length === 0) {
+        return
+    }
+    let insertSql = "insert into tron_bet_event.user_integral(addr,integral) values ? " +
+        " on duplicate key update integral=integral+values(integral)";
+    let insertResult = await db.query(insertSql, [dataList])
+    console.log("save user integral complete,result:" + insertResult)
+}
+
+/**
+ * 批量保存用户燃料信息
+ *
+ * @param dataList 用户燃料信息列表
+ * @returns {Promise<void>}
+ */
+async function saveUserFlight(dataList) {
+    if (!dataList || dataList.length === 0) {
+        return
+    }
+    let insertSql = "insert into tron_bet_event.user_flight(addr,fuel,plant) values ? " +
+        "on duplicate key update fuel=fuel+values(fuel)"
+    let insertResult = await db.query(insertSql, [dataList])
+    console.log("save user flight complete,result:" + insertResult)
+}
+
+
+
 module.exports = {
-    saveUserBetLog
+    saveUserBetLog,
+    saveUserIntegral,
+    saveUserFlight
 }
