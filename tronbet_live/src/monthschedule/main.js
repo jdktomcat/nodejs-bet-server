ad65a47d042cd8b7a4778b4ecf96026d38c116f3
andrew.li <andrew.li@tron.network>
2020-06-18 14:38:45 +0800

    update

--- tronbet_live/src/monthschedule/main.js
+++ tronbet_live/src/monthschedule/main.js
@@ -1,10 +1,20 @@
+const fs = require("fs")
 const {sendMail} = require("./mailUtils")
 const coinspaid = require("./coinspaid1")
 const financial = require("./financial1")
 const financialDiv = require("./financialDiv1")
 const mail = ['andrew.li@tron.network', 'jason.zhang@tron.network']
 
+const deleteExcel = function (attachments) {
+    for (let e of attachments) {
+        const p = e.path
+        console.log("e.path___>", e.path)
+        fs.unlinkSync(p)
+    }
+}
+
 const coinspaidData = async function () {
+    console.log("debug,", __dirname)
     const attachments = await coinspaid()
     const p = {
         mail: mail,
@@ -12,6 +22,7 @@ const coinspaidData = async function () {
         title: "coinspaidData"
     }
     await sendMail(p)
+    deleteExcel(attachments)
 }
 
 const financialData = async function (startDate, endDate) {
@@ -38,12 +49,12 @@ const getMonth = function () {
     const now = new Date()
     let nowMonth = now.getUTCMonth() + 1
     nowMonth = String(nowMonth).length < 2 ? "0" + nowMonth : String(nowMonth)
-    let endString = now.getFullYear() + nowMonth + "01"
+    let endString = now.getFullYear() + "-" + nowMonth + "-" + "01"
     //
     now.setUTCMonth(now.getUTCMonth() - 1)
     let lastMonth = now.getUTCMonth() + 1
     lastMonth = String(lastMonth).length < 2 ? "0" + lastMonth : String(lastMonth)
-    let startString = now.getFullYear() + lastMonth + "01"
+    let startString = now.getFullYear() + "-" + lastMonth + "-" + "01"
     return {
         startDate: startString,
         endDate: endString,
@@ -52,12 +63,13 @@ const getMonth = function () {
 
 const main = function () {
     const schedule = require('node-schedule');
-    const a1 = schedule.scheduleJob('27 * * * *', async function () {
+    const a1 = schedule.scheduleJob('40 * * * *', async function () {
         console.log(new Date(), "test_month_schedule")
         const {startDate, endDate} = getMonth()
+        console.log(startDate, endDate)
         await coinspaidData()
-        await financialData(startDate, endDate)
-        await financialDivData(startDate, endDate)
+        // await financialData(startDate, endDate)
+        // await financialDivData(startDate, endDate)
     })
 }
 
