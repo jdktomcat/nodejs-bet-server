a95fe3d3526234f6c66040b0d53047dc5b35be52
andrew.li <andrew.li@tron.network>
2020-07-02 15:38:29 +0800

    update1

--- tronbet_live/z_excute/hotfix/update1.js
+++ tronbet_live/z_excute/hotfix/update1.js
@@ -1,11 +1,68 @@
-const scanDiceRestart = function () {
-    try {
-        const cmd = 'pm2 list'
-        const a = require("child_process").execSync(cmd).toString()
-        console.log("1111: \n", a)
-    } catch (e) {
-        console.log("2222: \n", e)
-    }
+const db = require("../../src/utils/dbUtil");
+
+const rawQuery = async function (sql, params) {
+    console.log(sql, params)
+    const res = await db.exec(sql, params)
+    console.log("affectedRows is : " + res.affectedRows + "\n")
+    return res
 }
 
-module.exports = scanDiceRestart()
+const sql1 = `CREATE TABLE tron_bet_event.mine_box (
+    log_id bigint(20) NOT NULL AUTO_INCREMENT,
+    addr varchar(64) DEFAULT NULL,
+    amount varchar(64) DEFAULT NULL,
+    currency varchar(64) DEFAULT NULL,
+    type varchar(64) DEFAULT NULL,
+    boxNum varchar(64) DEFAULT NULL,
+    ts bigint(20) DEFAULT NULL,
+    PRIMARY KEY (log_id),
+    KEY mine_box_addr_idx (addr)
+);`
+
+const sql2 = `CREATE TABLE tron_bet_event.mine_box_count (
+    log_id bigint(20) NOT NULL AUTO_INCREMENT,
+    addr varchar(64) DEFAULT NULL,
+    type varchar(64) DEFAULT NULL,
+    boxNum bigint(20) unsigned DEFAULT 0,
+    PRIMARY KEY (log_id),
+    KEY mine_box_count_addr_idx (addr)
+);`
+
+const sql3 = `CREATE TABLE tron_bet_event.mine_send_log (
+    log_id bigint(20) NOT NULL AUTO_INCREMENT,
+    addr varchar(64) DEFAULT NULL,
+    amount varchar(64) DEFAULT NULL,
+    currency varchar(64) DEFAULT NULL,
+    tx_id varchar(64) DEFAULT NULL,
+    ts bigint(20) DEFAULT NULL,
+    PRIMARY KEY (log_id),
+    KEY mine_send_log_addr_idx (addr)
+);`
+
+
+const sql4 = `CREATE TABLE tron_bet_event.mine_letter (
+    log_id bigint(20) NOT NULL AUTO_INCREMENT,
+    addr varchar(64) DEFAULT NULL,
+    letter_D bigint(20) unsigned DEFAULT 0,
+    letter_I bigint(20) unsigned DEFAULT 0,
+    letter_C bigint(20) unsigned DEFAULT 0,
+    letter_E bigint(20) unsigned DEFAULT 0,
+    letter_W bigint(20) unsigned DEFAULT 0,
+    letter_N bigint(20) unsigned DEFAULT 0,
+    letter_T bigint(20) unsigned DEFAULT 0,
+    letter_R bigint(20) unsigned DEFAULT 0,
+    letter_X bigint(20) unsigned DEFAULT 0,
+    letter_K bigint(20) unsigned DEFAULT 0,
+    PRIMARY KEY (log_id),
+    KEY mine_letter_addr_idx (addr)
+);`
+
+
+const test1 = async function(){
+    await rawQuery(sql1,[])
+    await rawQuery(sql2,[])
+    await rawQuery(sql3,[])
+    await rawQuery(sql4,[])
+}
+
+module.exports = test1()
