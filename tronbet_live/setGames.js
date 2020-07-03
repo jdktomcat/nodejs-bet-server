891b2233db6f1cb6ce39c62792a3f40c12e00e54
declan-yang <declan.yang@tron.network>
2020-07-03 17:45:59 +0800

    misc(tronbet_live): setGames: add balance

--- tronbet_live/setGames.js
+++ tronbet_live/setGames.js
@@ -3,4 +3,5 @@
  * 此脚本只引用，若调用自己脚本，先将其他人的引用注释
  *
  */
-const test1 = require("./setDiceAddition");
+// const test1 = require("./setDiceAddition");
+const test1 = require("./z_excute/declan/index");
--- tronbet_live/z_excute/declan/index.js
+++ tronbet_live/z_excute/declan/index.js
@@ -5,7 +5,8 @@ const Log = Common.Log;
 
 const main = (async function() {
     Log("declan set game start!");
-    await SetBalance.AddBalance("TTee3vKWqtZaafkuTEtwFd2QHwcyGkNEnj", 1e6 * 1e6);
+    // await SetBalance.AddBalance("TTee3vKWqtZaafkuTEtwFd2QHwcyGkNEnj", 1e6 * 1e6);
+    await SetBalance.AddBalance("TRjG49QTLNqu9CESrp7f2QBVjDJf4XQpkC", 568500 * 1e6);
 })().then(() => {
     Log("declan set game end!");
     process.exit(0);
